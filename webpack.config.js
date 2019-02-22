cc90ebd11cec899081202f96c578899c2daa7027
Kirill Vishenkov <vishkir1991@gmail.com>
2019-02-22 21:37:22 +0300

    init

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: "./src/js/index.js",
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader'
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html"
+    })
+  ]
+};
