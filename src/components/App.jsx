eb6e1461c721e758eba9e2e28e9969f8abb2ac44
Kirill Vishenkov <vishkir1991@gmail.com>
2019-02-23 18:23:11 +0300

    feat(App) close tabs, add tabs

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -16,20 +16,38 @@ const fixtures = {
   eslint: 'ESLint is an open source project originally created by Nicholas C. Zakas in June 2013. Its goal is to provide a pluggable linting utility for JavaScript.',
   webpack: 'At its core, webpack is a static module bundler for modern JavaScript applications.',
 };
-console.log(_.fill(1, 0))
+console.log(_.keys(fixtures), _.difference(['jest'], _.keys(fixtures)));
 
 class App extends React.Component {
   state = {
     tabIndex: 0,
-    tabsCount: 3,
+    tabs: ['jest'],
   };
+
+  handleAddClick = () => {
+    const { tabs } = this.state;
+    const restTabs = _.difference(_.keys(fixtures), tabs);
+    if (!restTabs.length) {
+      throw new Error('There is nothing to add');
+    }
+    this.setState({
+      tabs: [...tabs, _.head(restTabs)],
+    });
+  }
 
   handleTabSelect = (tabIndex) => {
     this.setState({ tabIndex });
   }
 
+  handleCloseClick = value => () => {
+    const { tabs } = this.state;
+    this.setState({
+      tabs: _.reject(tabs, tab => tab === value),
+    });
+  }
+
   render() {
-    const { tabIndex, tabsCount } = this.state;
+    const { tabIndex, tabs } = this.state;
 
     return (
       <div className="container">
@@ -38,18 +56,40 @@ class App extends React.Component {
           onSelect={this.handleTabSelect}
         >
           <TabList>
-            {_.map(_.fill(Array(tabsCount), 0), (value, index) => (
-              <Tab data-test="section-tab">{_.capitalize(_.keys(fixtures)[index])}</Tab>
+            {_.map(tabs, (value, index) => (
+              <Tab
+                data-test="section-tab"
+                data-test-selected-tab={index === tabIndex}
+              >
+                {_.capitalize(value)}
+                <button
+                  type="button"
+                  className="close"
+                  onClick={this.handleCloseClick(value)}
+                  disabled={tabs.length === 1}
+                >
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </Tab>
             ))}
-            {/* <Tab data-test="section-tab">Enzyme</Tab> */}
-            <button type="button" className="btn btn-primary btn-sm">+</button>
+
+            <button
+              type="button"
+              className="btn btn-primary btn-sm"
+              disabled={_.keys(fixtures).length === tabs.length}
+              onClick={this.handleAddClick}
+            >
+              +
+            </button>
           </TabList>
 
-          
-          {_.map(_.fill(Array(tabsCount), 0), (value, index) => (
+          {_.map(tabs, (value, index) => (
             <TabPanel>
-              <div data-test="section-content">
-                <p className="content content1">{_.values(fixtures)[index]}</p>
+              <div
+                data-test="section-content"
+                data-test-selected-tab={index === tabIndex}
+              >
+                <p className="content content1">{fixtures[value]}</p>
               </div>
             </TabPanel>
           ))}
