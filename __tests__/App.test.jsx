b28aa966fdf795a74c8dde560fd8678c8f8e2688
Kirill Vishenkov <vishkir1991@gmail.com>
2019-02-23 12:36:11 +0300

    refactor(App.test) remove useless tests

--- __tests__/App.test.jsx
+++ __tests__/App.test.jsx
@@ -1,35 +1,16 @@
 import React from 'react';
-import { shallow, mount } from 'enzyme';
+import { mount } from 'enzyme';
 import toJson from 'enzyme-to-json';
 
 import App from '../src/App';
 
-describe('Shallow suite', () => {
-  it('should be selectable by class "container"', () => {
-    expect(shallow(<App />).is('.container')).toBe(true);
-  });
-
-  it('should change tab on click', () => {
-    const wrapper = mount(<App />);
-
-    const tab2 = wrapper.find('[data-test="section-tab"]').at(2);
-    tab2.simulate('click');
-    expect(wrapper.find('p').length).toEqual(2);
-  });
-});
-
-
 describe('Snapshot suite', () => {
-  it('should render correctly', () => {
-    const wrapper = shallow(<App />);
-    expect(toJson(wrapper)).toMatchSnapshot();
-  });
-
   it('should change tab on click', () => {
     const wrapper = mount(<App />);
 
-    const tab2 = wrapper.find('[data-test="section-tab"]').at(2);
-    tab2.simulate('click');
-    expect(toJson(wrapper.render(), { mode: 'deep' })).toMatchSnapshot();
+    const tab = wrapper.find('[data-test="section-tab"]').at(2);
+    tab.simulate('click');
+    const tabContent = wrapper.find('[data-test="section-content"]');
+    expect(toJson(tabContent.render())).toMatchSnapshot();
   });
 });
--- src/App.jsx
+++ src/App.jsx
@@ -17,18 +17,22 @@ export default () => (
       </TabList>
 
       <TabPanel>
-        <p className="content content1">Jest is a delightful JavaScript Testing Framework with a focus on simplicity.</p>
+        <div data-test="section-content">
+          <p className="content content1">Jest is a delightful JavaScript Testing Framework with a focus on simplicity.</p>
+        </div>
       </TabPanel>
       <TabPanel>
-        <p>
-          Enzyme is a JavaScript Testing utility for React that makes
-          it easier to test your React Components output.
-          You can also manipulate, traverse, and in some ways simulate runtime given the output.
-        </p>
-        <p>
-          Enzyme API is meant to be intuitive and flexible by mimicking
-          jQuery API for DOM manipulation and traversal.
-        </p>
+        <div data-test="section-content">
+          <p>
+            Enzyme is a JavaScript Testing utility for React that makes
+            it easier to test your React Components output.
+            You can also manipulate, traverse, and in some ways simulate runtime given the output.
+          </p>
+          <p>
+            Enzyme API is meant to be intuitive and flexible by mimicking
+            jQuery API for DOM manipulation and traversal.
+          </p>
+        </div>
       </TabPanel>
     </Tabs>
   </div>
