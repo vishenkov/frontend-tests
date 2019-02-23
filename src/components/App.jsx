aa342cab409864aff12ac6ef1e9f0935f1a6ae07
Kirill Vishenkov <vishkir1991@gmail.com>
2019-02-23 17:43:38 +0300

    feat(App) add fixtures --wip

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React from 'react';
 import {
   Tab,
@@ -5,52 +6,56 @@ import {
   TabList,
   TabPanel,
 } from 'react-tabs';
-
 import 'react-tabs/style/react-tabs.css';
+
 import withStateLogger from '../hocs/withStateLogger';
+
+const fixtures = {
+  jest: 'Jest is a delightful JavaScript Testing Framework with a focus on simplicity.',
+  enzyme: 'Enzyme is a JavaScript Testing utility for React that makes it easier to test your React Components output. You can also manipulate, traverse, and in some ways simulate runtime given the output.',
+  eslint: 'ESLint is an open source project originally created by Nicholas C. Zakas in June 2013. Its goal is to provide a pluggable linting utility for JavaScript.',
+  webpack: 'At its core, webpack is a static module bundler for modern JavaScript applications.',
+};
+console.log(_.fill(1, 0))
 
 class App extends React.Component {
   state = {
-    hello: 0
+    tabIndex: 0,
+    tabsCount: 3,
   };
 
-  handleClick = () => {
-    this.setState({
-      hello: this.state.hello + 1
-    });
+  handleTabSelect = (tabIndex) => {
+    this.setState({ tabIndex });
   }
 
   render() {
-    return (
-    <div className="container">
-    {this.state.hello}
-      <Tabs>
-        <TabList>
-          <Tab data-test="section-tab">Jest</Tab>
-          <Tab data-test="section-tab" onClick={this.handleClick}>Enzyme</Tab>
-        </TabList>
+    const { tabIndex, tabsCount } = this.state;
 
-        <TabPanel>
-          <div data-test="section-content">
-            <p className="content content1">Jest is a delightful JavaScript Testing Framework with a focus on simplicity.</p>
-          </div>
-        </TabPanel>
-        <TabPanel>
-          <div data-test="section-content">
-            <p>
-              Enzyme is a JavaScript Testing utility for React that makes
-              it easier to test your React Components output.
-              You can also manipulate, traverse, and in some ways simulate runtime given the output.
-            </p>
-            <p>
-              Enzyme API is meant to be intuitive and flexible by mimicking
-              jQuery API for DOM manipulation and traversal.
-            </p>
-          </div>
-        </TabPanel>
-      </Tabs>
-    </div>
-  );
+    return (
+      <div className="container">
+        <Tabs
+          selectedIndex={tabIndex}
+          onSelect={this.handleTabSelect}
+        >
+          <TabList>
+            {_.map(_.fill(Array(tabsCount), 0), (value, index) => (
+              <Tab data-test="section-tab">{_.capitalize(_.keys(fixtures)[index])}</Tab>
+            ))}
+            {/* <Tab data-test="section-tab">Enzyme</Tab> */}
+            <button type="button" className="btn btn-primary btn-sm">+</button>
+          </TabList>
+
+          
+          {_.map(_.fill(Array(tabsCount), 0), (value, index) => (
+            <TabPanel>
+              <div data-test="section-content">
+                <p className="content content1">{_.values(fixtures)[index]}</p>
+              </div>
+            </TabPanel>
+          ))}
+        </Tabs>
+      </div>
+    );
   }
 }
 
--- src/lib/log.js
+++ src/lib/log.js
@@ -1,6 +1,3 @@
 import debug from 'debug';
 
-export default (entity) => {
-  const debugLevel = `app:${entity}`;
-  return debug(debugLevel);
-};
+export default entity => debug(`app:${entity}`);
